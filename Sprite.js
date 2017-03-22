72cfd1f592310faf80a0ca1433d1c9b9e9de9a26
Victor M Torres <victor.torres@portalsge.com.br>
2017-03-22 20:15:24 -0300

    Adiciona aceleração

--- Sprite.js
+++ Sprite.js
@@ -1,9 +1,13 @@
 function Sprite(cor, x, y){
   this.x = x;
   this.y = y;
+  this.color = cor;
+
   this.vx = 0;
   this.vy = 0;
-  this.color = cor;
+  this.ax = 0;
+  this.ay = 0;
+
 }
 
 Sprite.prototype.desenhar = function (ctx) {
@@ -14,6 +18,9 @@ Sprite.prototype.desenhar = function (ctx) {
 };
 
 Sprite.prototype.mover = function (dt) {
+  this.vx = this.vx + this.ax * dt;
+  this.vy = this.vy + (this.ay + 60) * dt;
+
   this.x = this.x  + this.vx * dt;
   this.y = this.y  + this.vy * dt;
 };
